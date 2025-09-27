449a79cb19808a22ef8125a7acf36f8c460222e6
pentonfreak <antonmcmilan@gmail.com>
2025-09-26 20:26:03 -0400

    struggling to make it griddy

--- main.js
+++ main.js
@@ -4,3 +4,25 @@
 
 
 //Create a grid
+
+const TILE_SIZE = 32;
+const COLS = 15;
+const ROWS = 20;
+const WIDTH = TILE_SIZE * COLS;
+const HEIGHT = TILE_SIZE * ROWS;
+
+window.addEventListener("load", function() {
+    const canvas = document.getElementById("canvas1");
+    canvas.width = WIDTH;
+    canvas.height = HEIGHT;
+    
+    function drawGrid() {
+        for (let row = 0; row < ROWS; row++) {
+            for (let col = 0; col < COLS; col++) {
+                ctx.strokeRect(row * TILE_SIZE, col *  TILE_SIZE, TILE_SIZE, TILE_SIZE);
+            
+            }
+        }
+    }
+    drawGrid();
+});
