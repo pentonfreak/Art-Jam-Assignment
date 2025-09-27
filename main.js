5a8b54a37d6992d70bf5dd2ae8f555ced40b99b5
pentonfreak <antonmcmilan@gmail.com>
2025-09-26 20:08:29 -0400

    create grid

--- main.js
+++ main.js
@@ -1,22 +1,6 @@
-import { Hero } from "./hero.js";
-import { World } from "./script/world.js";
+//This is the Art Jam Assignment
+//By: Khuong Nguyen
+//Date: September 26, 2025
 
-export const TILE_SIZE = 32;
-export const COLS = 15;
-export const ROWS = 20;
-const GAME_WIDTH = TILE_SIZE * COLS;
-const GAME_HEIGHT = TILE_SIZE * ROWS;
 
-window.addEventListener('load', () => {
-    const canvas = document.getElementById('canvas1');
-    const ctx = canvas.getContext('2d');
-    canvas.width = GAME_WIDTH;
-    canvas.height = GAME_HEIGHT;
-
-    const world = new World();
-    world.drawGrid(ctx);
-
-    const hero = new Hero({});
-    hero.draw(ctx);
-
-});
+//Create a grid
