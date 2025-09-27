3be3731b73b8320595011082dfcedc8828e400c7
pentonfreak <antonmcmilan@gmail.com>
2025-09-26 20:33:34 -0400

    Grid Created

--- main.js
+++ main.js
@@ -15,14 +15,23 @@ window.addEventListener("load", function() {
     const canvas = document.getElementById("canvas1");
     canvas.width = WIDTH;
     canvas.height = HEIGHT;
-    
+    const ctx = canvas.getContext("2d");
+
     function drawGrid() {
         for (let row = 0; row < ROWS; row++) {
             for (let col = 0; col < COLS; col++) {
-                ctx.strokeRect(row * TILE_SIZE, col *  TILE_SIZE, TILE_SIZE, TILE_SIZE);
+                ctx.strokeRect(col * TILE_SIZE, row *  TILE_SIZE, TILE_SIZE, TILE_SIZE);
             
             }
         }
     }
     drawGrid();
 });
+
+//Draw self portrait
+//Using simple shapes and colors
+//In a pixel art style
+function drawPixelArt() {
+    //Head
+    ctx.fillStyle = "#f5cfa0";
+}
